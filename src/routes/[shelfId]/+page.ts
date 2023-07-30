8c9d4cb78b803fe6b6b244939e39717c8860d07f
diego-rapoport <diegomrapoport@gmail.com>
2023-07-29 23:27:45 -0300

    fix: redirect no id provided

--- src/routes/[shelfId]/+page.ts
+++ src/routes/[shelfId]/+page.ts
@@ -1,6 +1,7 @@
 import { SkoobService } from '$lib/skoob.services'
 import type { Book, PagingMetaData } from '$lib/skoob.types'
 import { id } from '../stores'
+import { redirect } from '@sveltejs/kit'
 
 export async function load({
 	params
@@ -18,7 +19,8 @@ export async function load({
 	let books: Book[] = []
 	let meta: PagingMetaData
 
-	const res = await skoobService.getBooks(myId!, { shelfId: parseInt(shelfId), limit: 10 })
+	if (!myId) throw redirect(307, '/')
+	const res = await skoobService.getBooks(myId, { shelfId: parseInt(shelfId), limit: 10 })
 	if (res.success) {
 		books = res.response
 		meta = res.paging
